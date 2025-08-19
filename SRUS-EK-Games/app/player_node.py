6ef0cf7835c25e83107c303e5ef279381c18cd17
Ewan-K20132799 <20132799@tafe.wa.edu.au>
2025-08-19 14:16:09 +0800

    Python files for step 2 and 3 partially done

--- SRUS-EK-Games/app/player_node.py
+++ SRUS-EK-Games/app/player_node.py
@@ -1,11 +1,19 @@
-
+from player import Player
 class PlayerNode:
 
     value: int
-    next_node: Node | None
+    next_node: None
+    Node: None
 
     def __init__(self,
                  value=int,
                  _next: Node | None = None):
         self._value = value
-        self.
+        self.next = _next
+
+    @property
+    def value(self):
+        return(Player)
+
+    def __str__(self):
+        return str(self.value)
