1eb172e5fee93b45be39c8a8b7510f457ad88e39
Ewan-K20132799 <20132799@tafe.wa.edu.au>
2025-08-26 13:49:56 +0800

    Updates to test_hash_map.py

--- test/test_hash_map.py
+++ test/test_hash_map.py
@@ -6,8 +6,45 @@ import app.player
 
 import app.player_list
 
+
 class TestPlayerHashMap(unittest.TestCase):
 
+    def test_get_item(self):
+
+        pass
+
+    def test_get_item_fail(self):
+
+        pass
+
+    def test_set_item(self):
+
+        pass
+
+    def test_set_item_fail(self):
+
+        pass
+
+    def test_del_item(self):
+
+        pass
+
+    def test_del_item_fail(self):
+
+        pass
+
+    def test_new_hash(self):
+
+        pass
+
+    def test_new_hash_fail(self):
+
+        pass
+
     def test_hash_display(self):
+
+        pass
+
+    def test_hash_display_fail(self):
 
         pass
