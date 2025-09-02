77dc9370710a69349be8b9ab43143552264211ed
Ewan-K20132799 <20132799@tafe.wa.edu.au>
2025-09-02 11:10:19 +0800

    Added hash.py

--- app/player_list.py
+++ app/player_list.py
@@ -1,5 +1,5 @@
 from app.player_node import LinkedPlayerNode
-from app.player import Player
+
 
 def is_empty():
    if LinkedPlayerNode is None:
@@ -58,66 +58,3 @@ class PlayerList: # Player list class
             return self._head.display(forward)
         else:
             return self._tail.display(forward)
-
-class PlayerHashMap:   # initiation of hash map class
-    SIZE: int = 10
-    def __init__(self, key: str | Player):
-        self.hashmap = [PlayerList(key)] % self.SIZE
-
-
-    def __hash__(self):
-
-        return
-
-
-
-
-    def __getitem__(self, key: str | Player) -> int:
-        if isinstance(key, Player):
-            return hash(key) % self.SIZE
-        else:
-            return Player.player_hash_function(key) % self.SIZE
-
-
-
-
-
-    def __setitem__(self,
-                    key: str,
-                    name: str,
-                    index: str | Player) -> None:
-
-
-        if key in index:
-            _name = ""
-            return
-        else:
-
-            _name = ""
-            return
-
-
-    def __len__(self, key: str) -> int:
-
-
-
-
-
-
-
-
-    def __delitem__(self, key: str, name: str):
-
-
-
-
-
-
-
-
-
-    def display(self,
-                key: str,
-                name: str | Player) -> int:
-
-
