ff6a41314cc24f04acfe8a8b25ed26dec225e1d0
Ewan-K20132799 <20132799@tafe.wa.edu.au>
2025-09-16 13:08:09 +0800

    feat: POR3 added scores

--- app/player.py
+++ app/player.py
@@ -1,10 +1,13 @@
 class Player:  # Player class
 
     def __init__(self,
-                 uid=str,
-                 name=str):  # init function for unique identifier and name
+                 uid,
+                 name,
+                 score=int(0)):  # init function for unique identifier and name
+
         self._uid = uid  # private function for uid
         self._name = name  # private function for name
+        self._score = score # private function for score
 
     def uid(self):
         return self._uid
@@ -13,7 +16,10 @@ class Player:  # Player class
         return self._name
 
     def score(self):
-
+        if self._score < 0:
+            raise ValueError
+        else:
+            return self._score
 
     def __str__(self):  # String function for returning uid and name
         return self  # Returns the string of uid and name
@@ -27,3 +33,14 @@ class Player:  # Player class
 
     def __eq__(self, other):
         return self.uid == other.uid
+
+    def __get__(self, score: int):
+        return self._score
+
+    def __set__(self, score: int):
+        return self._score
+
+    def __repr__(self):
+        self.__class__.__name__ = [Player('01', 'Alice', 10)]
+        return self.__class__.__name__
+
--- test/player_test.py
+++ test/player_test.py
@@ -4,10 +4,11 @@ import sys # Sys import for accessing app directory for test
 
 import app.player
 
-sys.path.insert(0,'../Source/Repo/SRUS-EK-Games/SRUS-EK-Games/')
+sys.path.insert(0,'../Source/Repo/SRUS-EK-Games-POR3/')
 # Sys path insert for
 
 from app.player import Player # Import statement for Player class
+
 
 class TestPlayer(unittest.TestCase):  # Test class for player.py module
 
@@ -18,3 +19,28 @@ class TestPlayer(unittest.TestCase):  # Test class for player.py module
     def test_name_returns_str(self):  # Tests property from Player class
         name_t = app.player.Player.name  # name_t is equal to name in Player class
         return self, name_t  # Assertion that name from Player class is a string
+
+    def test_get_score(self):
+        get = app.player.Player.__get__
+        if get != int:
+            assert ValueError
+        else:
+            assert get == int
+
+    def test_set_score(self):
+        set = app.player.Player.__set__
+        if set != int:
+            assert ValueError
+        else:
+            assert set == int
+
+    def test_score_returns_int(self):
+        score_t = app.player.Player.score
+        return self, score_t
+
+    def test_repr_return(self):
+        repr_state = app.player.Player.__name__
+        if repr_state == [Player('01', 'Alice', 10)]:
+            return repr_state
+        else:
+            raise ValueError
